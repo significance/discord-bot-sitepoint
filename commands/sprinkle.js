c5312e33b566fd6587951c94dd35bd73532f0966
wendydv1989 <wendydv1989@gmail.com>
2021-02-22 23:17:51 +0100

    hi there it`s done

--- commands/sprinkle.js
+++ commands/sprinkle.js
@@ -1,17 +1,25 @@
 const axios = require('axios')
 const FAUCETSECRET = process.env.FAUCETSECRET;
+let wazSent = {};
+let sendAlways = false
 
 async function callFaucet(arg) {
   return axios.post('https://faucet.ethswarm.org/fund-gbzz', {
     token: FAUCETSECRET,
     receiver: arg
   }).then(res => {
-    console.log(`statusCode: ${res.statusCode}`)
-    console.log(res.data.success)
-    return res.data.success
+    console.log(`statusCode:`, res.data)
+    if (res.data.success) {
+      console.log(res.data.success)
+      wazSent[arg] === undefined ? wazSent[arg] = 1 : wazSent[arg] += 1
+      return res.data.success.faucetAddress
+    } else {
+      console.log(res.data.error)
+      return "Something went terribly wrong."
+    }
   }).catch(error => {
-    console.error(error)
-    return error
+    console.error('error!', error)
+    return "Faucet calling error"
   })
 }
 
@@ -19,10 +27,14 @@ module.exports = {
   name: 'sprinkle',
   description: 'Sprinkle',
   execute(msg, args) {
-    msg.reply('hold on... dripping to ' + args[0]);
-    callFaucet(args[0]).then((result) => {
-      msg.channel.send(result.faucetAddress);
-    })
+    msg.reply('hold on... checking out ' + msg.author.username + ' spam status');
+    if (wazSent[args[0]] !== undefined && (wazSent[args[0]] > 0 && sendAlways !== true)) {
+      msg.channel.send('You sprinkled this address already ' + msg.author.username + ' you naughty you!');
+    } else {
+      callFaucet(args[0]).then((result) => {
+        msg.channel.send(result);
+      })
+    }
   },
 };
 
